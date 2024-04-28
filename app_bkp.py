d80cc3c31140b03912594818e031ab4f1afe45b2
Chandan Vijayashankar <chandan09121995@gmail.com>
2024-04-28 14:40:24 +0530

    using ViTImageProcessor method, with app bkp

--- app_bkp.py
+++ app_bkp.py
@@ -1,15 +1,17 @@
-from transformers import VisionEncoderDecoderModel, ViTImageProcessor, AutoTokenizer
+import streamlit as st
+from transformers import VisionEncoderDecoderModel, ViTFeatureExtractor, AutoTokenizer
 import torch
 from PIL import Image
-import streamlit as st
 
+# Load the pre-trained model and tokenizer
 model = VisionEncoderDecoderModel.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
-image_processor = ViTImageProcessor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
+feature_extractor = ViTFeatureExtractor.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
 tokenizer = AutoTokenizer.from_pretrained("nlpconnect/vit-gpt2-image-captioning")
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model.to(device)
 
+# Set the parameters for caption generation
 max_length = 16
 num_beams = 4
 gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
@@ -17,15 +19,8 @@ gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
 def predict_step(image):
     if image is None:
         return []
-    # images = []
-    # for image_path in image_paths:
-    #     i_image = Image.open(image_path)
-    #     if i_image.mode != "RGB":
-    #         i_image = i_image.convert(mode="RGB")
 
-    #     images.append(i_image)
-
-    pixel_values = image_processor(images=[image], return_tensors="pt").pixel_values
+    pixel_values = feature_extractor(images=[image], return_tensors="pt").pixel_values
     pixel_values = pixel_values.to(device)
 
     output_ids = model.generate(pixel_values, **gen_kwargs)
